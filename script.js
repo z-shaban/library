5d7f055d747edc2b6422c60cf35210c20998bca5
shaban <146794146+sarzhh@users.noreply.github.com>
2024-11-10 20:26:35 +0000

    Add logic to toggle between read and not read

--- script.js
+++ script.js
@@ -17,6 +17,15 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+Book.prototype.toggle = function(){
+    if (this.read == "yes"){
+        this.read = "no"
+    }
+    else{
+        this.read = "yes";
+    }
+}
+
 function addBookToLibrary(title, author, pages, read){
     let newBook= new Book (title, author, pages, read);
 
@@ -32,28 +41,37 @@ function displayBooks(){
         const bookTitle = document.createElement("h3");
         const bookAuthor = document.createElement("p");
         const bookPages = document.createElement("p");
-        const bookRead = document.createElement("p");
+        const bookRead = document.createElement("div");
+        const readText = document.createElement("span");
+        const readButton = document.createElement("button");
         const remove = document.createElement("button")
 
         bookCard.setAttribute("data-index", index);
 
-        bookTitle.textContent = book.title;
-        bookAuthor.textContent = book.author;
-        bookPages.textContent = book.pages;
-        bookRead.textContent = book.read;
+        bookTitle.textContent ="TITLE: " + book.title;
+        bookAuthor.textContent = "AUTHOR: " + book.author;
+        bookPages.textContent = "NUMBER OF PAGES: " + book.pages;
+        readText.textContent = "READ: ";
+        readButton.textContent = book.read;
         remove.textContent = "REMOVE";
 
+        bookRead.append(readText, readButton);
         bookCard.append(bookTitle, bookAuthor, bookPages, bookRead, remove);
         main.appendChild(bookCard);
         
         const myIndex = parseInt(bookCard.getAttribute("data-index"), 10);
 
         remove.addEventListener("click", ()=>{
-            let newLibrary = myLibrary.filter((book, index) => !index == myIndex);
+            let newLibrary = myLibrary.filter((book, index) => index !== myIndex);
             myLibrary = newLibrary;
             console.log(newLibrary);
             bookCard.remove();
-        })
+        });
+
+        readButton.addEventListener("click", ()=>{
+            book.toggle();
+            readButton.textContent = book.read;
+        });
     });
 
 };
@@ -75,6 +93,6 @@ add.addEventListener("click",(e) =>{
 
 
 
-addBookToLibrary("harry porter", "shaban", "293", "read");
+addBookToLibrary("harry porter", "shaban", "293", "yes");
 
 
